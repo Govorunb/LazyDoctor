49069927ae1e3667a046c1956eb6b5383816f11c
Govorunb <Govorunb@users.noreply.github.com>
2025-01-18 12:23:03 +1100

    fix justice knight breaking the app

--- DesktopApp/Data/Operators/OperatorRepository.cs
+++ DesktopApp/Data/Operators/OperatorRepository.cs
@@ -22,8 +22,28 @@ public sealed class OperatorRepository : DataSource<IReadOnlyCollection<Operator
             .DisposeWith(this);
     }
 
-    public Operator? GetById(string id) => _byId?[id];
-    public Operator? GetByName(string name) => _byName?[name];
+    public Operator? GetById(string id)
+    {
+        if (_byId is null) return null;
+        if (_byId.TryGetValue(id, out var result)) return result;
+
+        this.Log().Error($"Operator ID not found: {id}");
+        return null;
+    }
+
+    public Operator? GetByName(string name)
+    {
+        if (_byName is null) return null;
+
+        if (_byName.TryGetValue(name, out var op)) return op;
+
+        if (_aliases.TryGetValue(name, out var aliasedName) && _byName.TryGetValue(aliasedName, out op))
+            return op;
+
+        this.Log().Error($"Operator name not found: {name}");
+        return null;
+    }
+
     public override Task Reload() => _source.Reload();
 
     private IReadOnlyCollection<Operator> Process(Dictionary<string, Operator> ops)
@@ -40,4 +60,9 @@ public sealed class OperatorRepository : DataSource<IReadOnlyCollection<Operator
 
         return _byId.Values;
     }
+
+    private static readonly Dictionary<string, string> _aliases = new()
+    {
+        ["Justice Knight"] = "'Justice Knight'",
+    };
 }
