eb42765cf011eb126ed1e1c823456d0ce4eee05e
Govorunb <Govorunb@users.noreply.github.com>
2025-05-23 23:18:48 +1000

    i should really just make a library

--- DesktopApp/Utilities/Converters/ValueConverterBase.cs
+++ DesktopApp/Utilities/Converters/ValueConverterBase.cs
@@ -4,15 +4,21 @@ using Avalonia.Data.Converters;
 
 namespace DesktopApp.Utilities.Converters;
 
-public abstract class ValueConverterBase : IValueConverter
+public abstract class ConverterBase
+{
+    protected static BindingNotification Error(string message, string? paramName = null)
+        => new(new ArgumentException(message, paramName), BindingErrorType.DataValidationError);
+}
+
+public abstract class ValueConverterBase : ConverterBase, IValueConverter
 {
     public abstract object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture);
 
     public virtual object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotSupportedException();
-    }
+        => throw new NotSupportedException();
+}
 
-    protected static BindingNotification Error(string message, string? paramName = null)
-        => new(new ArgumentException(message, paramName), BindingErrorType.DataValidationError);
+public abstract class MultiValueConverterBase : ConverterBase, IMultiValueConverter
+{
+    public abstract object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture);
 }
